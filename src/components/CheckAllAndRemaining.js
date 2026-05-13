c038bcc1fbc2ba8c59a2041b93f7ed9983b4451a
Aung Naing <aungnaing.brian@gmail.com>
2026-05-14 02:36:16 +0400

    c7ep40

--- src/components/CheckAllAndRemaining.js
+++ src/components/CheckAllAndRemaining.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function CheckAllRemaining() {
+export default function CheckAllAndRemaining() {
   return (
     <div className="check-all-container">
           <div>
